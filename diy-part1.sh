24ffc07e29edec7a0f40d5b9fc12b2302a46db44
歌者 <34333540+hxh1@users.noreply.github.com>
2024-10-22 00:32:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,10 +8,4 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
 
