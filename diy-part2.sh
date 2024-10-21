6f01ef792584579bdeec2dfd1ca1dfc0b4383283
歌者 <34333540+hxh1@users.noreply.github.com>
2024-10-21 22:24:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,6 +20,19 @@ sed -i 's/192.168.1.1/192.168.8.8/g' package/base-files/files/bin/config_generat
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 
+#替换argon主题
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+#rm -rf feeds/luci/themes/luci-theme-argon
+#rm -rf feeds/luci/themes/luci-theme-design
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+#git clone https://github.com/gngpp/luci-theme-design.git feeds/luci/themes/luci-theme-design
+#rm -rf feeds/luci/applications/luci-app-argon-config
+#rm -rf feeds/luci/applications/luci-app-design-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+#git clone https://github.com/gngpp/luci-app-design-config.git feeds/luci/applications/luci-app-design-config
+
+
 #将clash内核、TUN内核、Meta内核编译进目录
 mkdir -p files/etc/openclash/core
 curl -L https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz | tar -xz -C /tmp
