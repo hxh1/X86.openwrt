1fac6965fbe48aa7239828dbe4419900ebd10c24
歌者 <34333540+hxh1@users.noreply.github.com>
2024-10-22 19:20:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,14 +21,14 @@ sed -i 's/192.168.1.1/192.168.8.8/g' package/base-files/files/bin/config_generat
 
 
 #替换argon主题
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 #git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-#rm -rf feeds/luci/themes/luci-theme-argon
 #rm -rf feeds/luci/themes/luci-theme-design
 #git clone https://github.com/gngpp/luci-theme-design.git feeds/luci/themes/luci-theme-design
-#rm -rf feeds/luci/applications/luci-app-argon-config
 #rm -rf feeds/luci/applications/luci-app-design-config
 #git clone https://github.com/gngpp/luci-app-design-config.git feeds/luci/applications/luci-app-design-config
 
